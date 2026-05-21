350c996ac1b75a58b0f9245d68f546a2169a2bd4
Ashish Pal <ashishpal0502@gmail.com>
2026-05-21 19:47:42 +0530

    hero banner updated

--- src/components/HeroHome/HeroHome.js
+++ src/components/HeroHome/HeroHome.js
@@ -5,14 +5,18 @@ import Link from "next/link";
 export default function HeroHome() {
   return (
     <main className="text-white">
-      {/* HERO SECTION */}
-      <section className="relative h-screen w-full flex items-center">
-        {/* BACKGROUND IMAGE */}
-        <img
-          src="https://images.unsplash.com/photo-1581092580497-e0d23cbdf1dc"
-          alt="robotics"
-          className="absolute inset-0 w-full h-full object-cover"
-        />
+  
+      <section className="relative h-screen w-full flex items-center"> 
+<video
+  autoPlay
+  muted
+  loop
+  playsInline
+  preload="metadata"
+  className="absolute inset-0 w-full h-full object-cover"
+>
+  <source src="/mp4/HeroV1.mp4" type="video/mp4" />
+</video>
 
         {/* DARK OVERLAY */}
         <div className="absolute inset-0 bg-black/60"></div>
@@ -21,23 +25,23 @@ export default function HeroHome() {
         <div className="relative z-10 max-w-7xl mx-auto px-6 w-full">
           <div className="max-w-2xl">
             {/* TRUST BADGE */}
-            <div className="flex items-center gap-3 mb-6">
+            <div className="flex items-center gap-3 mb-2">
               <div className="flex -space-x-2">
-                <div className="w-8 h-8 bg-green-500 rounded-full border-2 border-white"></div>
-                <div className="w-8 h-8 bg-green-500 rounded-full border-2 border-white"></div>
-                <div className="w-8 h-8 bg-green-500 rounded-full border-2 border-white"></div>
+                <div className="w-8 h-8 bg-[#083427] rounded-full border-2 border-white"></div>
+                <div className="w-8 h-8 bg-[#083427] rounded-full border-2 border-white"></div>
+                <div className="w-8 h-8 bg-[#083427] rounded-full border-2 border-white"></div>
               </div>
               <p className="text-sm text-gray-300">1K+ Trusted Customers</p>
             </div>
 
             {/* HEADING */}
-            <h1 className="text-4xl md:text-6xl font-bold leading-tight mb-6">
+            <h1 className="text-xl md:text-5xl font-bold leading-tight mb-2">
               Control Redefined <br />
               Innovation Unleashed
             </h1>
 
             {/* DESCRIPTION */}
-            <p className="text-gray-300 mb-8">
+            <p className="text-gray-300 mb-3">
               We focus on building intelligent robots for real-world logistics
               and industrial inspection, starting with Warehouse AMRs and
               Quadruped Inspection Robots.
@@ -66,3 +70,4 @@ export default function HeroHome() {
     </main>
   );
 }
+
